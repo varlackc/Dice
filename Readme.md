bb55b185c0dfd290c12dc60d6611a8881b795b75
varlackc <varlackc@mail.gvsu.edu>
2025-08-14 23:52:30 -0500

    initial test and documentation

---
+++ Readme.md
@@ -0,0 +1,18 @@
+# Documentation for the Dice Project
+---
+
+The purpose of the dice project is to facilitate the process of generating random numbers.
+
+The die project should provide the user with the ability to select the type of die that they want to use.
+
+https://en.wikipedia.org/wiki/Dice
+
+Some of the die options that are available include D4, D6, D8, D10, D12, and D20. There is also the possibility to create a custom die.
+
+The program should allow for rolling a die to revil the new random number, shake a die to generate a random number before rolling, get the current face of a die, to get the type of die that is being used or to set the face of a die.
+
+The test for the die program have been written using the default unittest library that comes preinstalled in python. 
+
+We can run the tests using the following command:
+
+`python -m unittest`
---
+++ test_die.py
@@ -0,0 +1,132 @@
+# unittest library documentation for python version 3.7
+# https://docs.python.org/3.7/library/unittest.html
+import unittest
+from die import *
+
+# to test the whole thing python -m unittest
+class TestD2(unittest.TestCase):
+    # to test run python -m unittest test_die
+    def test_roll(self):
+        # verify an output is present
+        self.assertIsNotNone(D2().roll())
+    def test_low_bound(self):
+        # verify the output does not exceed lower bound
+        self.assertGreater(D2().roll(),0)
+    def test_upper_bound(self):
+        # verify the output does not exceed upper bound
+        self.assertLess(D2().roll(),3)
+    def test_shake(self):
+        # verify the shake method does not output
+        self.assertIsNone(D2().shake())
+    def test_sides(self):
+        self.assertEqual(D2().side_number, 2)
+    def test_die_Type(self):
+        self.assertEqual(D2().die_Type, "d2")
+class TestD4(unittest.TestCase):
+    # to test run python -m unittest test_die
+    def test_roll(self):
+        # verify an output is present
+        self.assertIsNotNone(D4().roll())
+    def test_low_bound(self):
+            # verify the output does not exceed lower bound
+        self.assertGreater(D4().roll(),0)
+    def test_upper_bound(self):
+        # verify the output does not exceed upper bound
+        self.assertLess(D4().roll(),5)
+    def test_shake(self):
+        # verify the shake method does not output
+        self.assertIsNone(D4().shake())
+    def test_sides(self):
+        self.assertEqual(D4().side_number, 4)
+    def test_die_Type(self):
+        self.assertEqual(D4().die_Type, "d4")
+class TestD6(unittest.TestCase):
+    # to test run python -m unittest test_die
+    def test_roll(self):
+        # verify an output is present
+        self.assertIsNotNone(D6().roll())
+    def test_low_bound(self):
+        # verify the output does not exceed lower bound
+        self.assertGreater(D6().roll(),0)
+    def test_upper_bound(self):
+        # verify the output does not exceed upper bound
+        self.assertLess(D6().roll(),7)
+    def test_shake(self):
+        # verify the shake method does not output
+        self.assertIsNone(D6().shake())
+    def test_sides(self):
+        self.assertEqual(D6().side_number, 6)
+    def test_die_Type(self):
+        self.assertEqual(D6().die_Type, "d6")
+class TestD8(unittest.TestCase):
+    # to test run python -m unittest test_die
+    def test_roll(self):
+        # verify an output is present
+        self.assertIsNotNone(D8().roll())
+    def test_low_bound(self):
+        # verify the output does not exceed lower bound
+        self.assertGreater(D8().roll(),0)
+    def test_upper_bound(self):
+        # verify the output does not exceed upper bound
+        self.assertLess(D8().roll(),9)
+    def test_shake(self):
+        # verify the shake method does not output
+        self.assertIsNone(D8().shake())
+    def test_sides(self):
+        self.assertEqual(D8().side_number, 8)
+    def test_die_Type(self):
+        self.assertEqual(D8().die_Type, "d8")
+class TestD10(unittest.TestCase):
+    # to test run python -m unittest test_die
+    def test_roll(self):
+        # verify an output is present
+        self.assertIsNotNone(D10().roll())
+    def test_low_bound(self):
+        # verify the output does not exceed lower bound
+        self.assertGreater(D10().roll(),0)
+    def test_upper_bound(self):
+        # verify the output does not exceed upper bound
+        self.assertLess(D10().roll(),11)
+    def test_shake(self):
+        # verify the shake method does not output
+        self.assertIsNone(D10().shake())
+    def test_sides(self):
+        self.assertEqual(D10().side_number, 10)
+    def test_die_Type(self):
+        self.assertEqual(D10().die_Type, "d10")
+class TestD12(unittest.TestCase):
+    # to test run python -m unittest test_die
+    def test_roll(self):
+        # verify an output is present
+        self.assertIsNotNone(D12().roll())
+    def test_low_bound(self):
+        # verify the output does not exceed lower bound
+        self.assertGreater(D12().roll(),0)
+    def test_upper_bound(self):
+        # verify the output does not exceed upper bound
+        self.assertLess(D12().roll(),13)
+    def test_shake(self):
+        # verify the shake method does not output
+        self.assertIsNone(D12().shake())
+    def test_sides(self):
+        self.assertEqual(D12().side_number, 12)
+    def test_die_Type(self):
+        self.assertEqual(D12().die_Type, "d12")
+class TestD20(unittest.TestCase):
+    # to test run python -m unittest test_die
+    def test_roll(self):
+        # verify an output is present
+        self.assertIsNotNone(D20().roll())
+    def test_low_bound(self):
+        # verify the output does not exceed lower bound
+        self.assertGreater(D20().roll(),0)
+    def test_upper_bound(self):
+        # verify the output does not exceed upper bound
+        self.assertLess(D20().roll(),21)
+    def test_shake(self):
+        # verify the shake method does not output
+        self.assertIsNone(D20().shake())
+    def test_sides(self):
+        self.assertEqual(D20().side_number, 20)
+    def test_die_Type(self):
+        self.assertEqual(D20().die_Type, "d20")
