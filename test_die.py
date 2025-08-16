64cede69e226a0cb73b46ed961cf9d589567efc6
varlackc <varlackc@mail.gvsu.edu>
2025-08-15 20:31:46 -0500

    added tests for custom sided dice

--- test_die.py
+++ test_die.py
@@ -130,3 +130,20 @@ class TestD20(unittest.TestCase):
         self.assertEqual(D20().side_number, 20)
     def test_die_Type(self):
         self.assertEqual(D20().die_Type, "d20")
+class TestCustomSides(unittest.TestCase):
+    def test_roll(self):
+            # verify an output is present
+        self.assertIsNotNone(CustomSides(100).roll())
+    def test_low_bound(self):
+        # verify the output does not exceed lower bound
+        self.assertGreater(CustomSides(100).roll(),0)
+    def test_upper_bound(self):
+        # verify the output does not exceed upper bound
+        self.assertLess(CustomSides(100).roll(),101)
+    def test_shake(self):
+        # verify the shake method does not output
+        self.assertIsNone(CustomSides(100).shake())
+    def test_sides(self):
+        self.assertEqual(CustomSides(100).side_number, 100)
+    def test_die_Type(self):
+        self.assertEqual(CustomSides(100).die_Type, "custom_sided")
