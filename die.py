b030fa676d5eab935eb2c1e4b20e602eface3610
varlackc <varlackc@mail.gvsu.edu>
2025-08-24 00:00:47 -0500

    Add a comment to the D2 Die Class

--- die.py
+++ die.py
@@ -37,6 +37,11 @@ class Die:
         if(value < self.side_number and value > 0):
             self.face = value
 class D2(Die):
+    """This class describes the D2 Die
+
+    Args:
+        Die (_type_): _description_
+    """
     def __init__(self):
         self.die_Type = "d2"
         self.side_number = 2
