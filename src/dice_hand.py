db69c24dcb59b7004745a19f44291009584b519f
varlackc <varlackc@mail.gvsu.edu>
2025-08-26 23:49:10 -0500

    Added the get_faces method

--- src/dice_hand.py
+++ src/dice_hand.py
@@ -3,6 +3,13 @@ from . import *
 class DiceHand:
     def __init__(self):
         self.die_set = []
+        self.die_faces = []
         self.count_total = 0
     def add_die(self, die):
         self.die_set.append(die)
+    def get_faces(self):
+        # reset the die faces
+        self.die_faces = []
+        for i in range(len(self.die_set)):
+            self.die_faces.append(self.die_set[i].get_face())
+        return self.die_faces
--- tests/test_dice.py
+++ tests/test_dice.py
@@ -4,7 +4,15 @@ from src import *
 class TestDiceHand(unittest.TestCase):
     def test_add_die(self):
         # verify that a die can be added
-        d1 = die
+        d1 = Die()
         set_d1 = DiceHand()
         set_d1.add_die(d1)
         self.assertGreater(len(set_d1.die_set),0)
+    def test_get_faces(self):
+        # verify that the faces can be shown
+        d1 = Die()
+        d2 = Die()
+        set_d1 = DiceHand()
+        set_d1.add_die(d1)
+        set_d1.add_die(d2)
+        self.assertIsNotNone(set_d1.get_faces())
