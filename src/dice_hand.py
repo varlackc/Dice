8170af4c0f121773823452146e1b7eaa203c4e6a
varlackc <varlackc@mail.gvsu.edu>
2025-08-28 23:24:42 -0500

    specify the output types in the dice hand class

--- src/dice_hand.py
+++ src/dice_hand.py
@@ -7,29 +7,29 @@ class DiceHand:
         self.die_set = []
         self.die_faces = []
         self.count_total = 0
-    def add_die(self, die):
+    def add_die(self, die) -> None:
         self.die_set.append(die)
     def add_dice(self, dice: List[Any]) -> None:
         # add multiple dice at once
         for i in range(len(dice)):
             self.die_set.append(dice[i])
-    def get_faces(self):
+    def get_faces(self) -> int:
         # reset the die faces
         self.die_faces = []
         for i in range(len(self.die_set)):
             self.die_faces.append(self.die_set[i].get_face())
         return self.die_faces
-    def shake_dice(self):
+    def shake_dice(self) -> None:
         # shake the dice
         for i in range(len(self.die_set)):
             self.die_set[i].shake()
-    def roll_dice(self):
+    def roll_dice(self) -> int:
         # roll the dice
         result = []
         for i in range(len(self.die_set)):
             result.append(self.die_set[i].roll())
         return result
-    def get_total(self):
+    def get_total(self) -> int:
         # get the dice count total
         result = 0
         for i in range(len(self.die_set)):
