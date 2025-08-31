238862d8dbabdc5ab5cfd6a212e06b1504b99f56
varlackc <varlackc@mail.gvsu.edu>
2025-08-31 15:50:00 -0500

    move the TestD8 class into a separate file

--- tests/test_die.py
+++ tests/test_die.py
@@ -39,28 +39,6 @@ class TestDie(unittest.TestCase):
         self.assertEqual(Die().side_number, 6)
     def test_die_Type(self):
         self.assertEqual(Die().die_Type, "d6")
-class TestD8(unittest.TestCase):
-    """This class describes the tests for an eight sided die
-
-    Args:
-        unittest (_type_): _description_
-    """
-    def test_roll(self):
-        # verify an output is present
-        self.assertIsNotNone(D8().roll())
-    def test_low_bound(self):
-        # verify the output does not exceed lower bound
-        self.assertGreater(D8().roll(),0)
-    def test_upper_bound(self):
-        # verify the output does not exceed upper bound
-        self.assertLess(D8().roll(),9)
-    def test_shake(self):
-        # verify the shake method does not output
-        self.assertIsNone(D8().shake())
-    def test_sides(self):
-        self.assertEqual(D8().side_number, 8)
-    def test_die_Type(self):
-        self.assertEqual(D8().die_Type, "d8")
 class TestD10(unittest.TestCase):
     """This class describes the tests for a ten sided die
 
