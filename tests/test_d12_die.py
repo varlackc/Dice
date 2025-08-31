19e800c8d969e0b306988c89d18489a9d436215f
varlackc <varlackc@mail.gvsu.edu>
2025-08-31 16:01:45 -0500

    moved the D12 test class to a separate file

---
+++ tests/test_d12_die.py
@@ -0,0 +1,25 @@
+import unittest
+from src import *
+
+class TestD12(unittest.TestCase):
+    """This class describes the tests for a twelve sided die
+
+    Args:
+        unittest (_type_): _description_
+    """
+    def test_roll(self):
+        # verify an output is present
+        self.assertIsNotNone(D12().roll())
+    def test_low_bound(self):
+        # verify the output does not exceed lower bound
+        self.assertGreater(D12().roll(),0)
+    def test_upper_bound(self):
+        # verify the output does not exceed upper bound
+        self.assertLess(D12().roll(),13)
+    def test_shake(self):
+        # verify the shake method does not output
+        self.assertIsNone(D12().shake())
+    def test_sides(self):
+        self.assertEqual(D12().side_number, 12)
+    def test_die_Type(self):
+        self.assertEqual(D12().die_Type, "d12")
