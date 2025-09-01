aeaba2dcdc8c37eaf99f23a6bcfca09e0ba3e04d
varlackc <varlackc@mail.gvsu.edu>
2025-09-01 07:48:08 -0500

    move the non numeric tests to a separate file

---
+++ tests/test_non_numeric.py
@@ -0,0 +1,24 @@
+import unittest
+from src import *
+
+class TestNonNumeric(unittest.TestCase):
+    """This class describes the tests for non numeric die
+
+    Args:
+        unittest (_type_): _description_
+    """
+    def test_roll(self):
+        # verify an output is present
+        self.assertIsNotNone(NonNumeric(["A","B","C","D","E","F"]).roll())
+    def test_string_output(self):
+        # verify the output is a string
+        self.assertEqual(type(NonNumeric(["A","B","C","D","E","F"]).roll()),type("A"))
+        # verify the output is an element in the list of sides
+        self.assertIn(NonNumeric(["A","B","C","D","E","F"]).roll(), ["A","B","C","D","E","F"])
+    def test_shake(self):
+        # verify the shake method does not output
+        self.assertIsNone(NonNumeric(["A","B","C","D","E","F"]).shake())
+    def test_sides(self):
+        self.assertEqual(NonNumeric(["A","B","C","D","E","F"]).side_number, len(["A","B","C","D","E","F"]))
+    def test_die_Type(self):
+        self.assertEqual(NonNumeric(["A","B","C","D","E","F"]).die_Type, "non-numeric")
