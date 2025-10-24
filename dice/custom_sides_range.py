2be7beae13d76dc38ed87efc516a3379ebd5565f
varlackc <varlackc@mail.gvsu.edu>
2025-10-24 00:15:43 -0500

    add type hints to the custom_sides_range class

--- dice/custom_sides_range.py
+++ dice/custom_sides_range.py
@@ -9,14 +9,14 @@ class CustomSidesRange(Die):
     Args:
         Die (_type_): _description_
     """
-    def __init__(self, lower: int, higher: int):
+    def __init__(self, lower: int, higher: int) -> None:
         """
         use is instance to verify that the sides is of type integer
         https://docs.python.org/3.7/library/functions.html#isinstance
         """
         if (isinstance(lower, int) and isinstance(higher, int)):
-            self.die_Type = "custom_sided_range"
-            self.face = lower
-            self.side_number = higher - lower
-            self.lower = lower
-            self.higher = higher
+            self.die_Type: str = "custom_sided_range"
+            self.face: int = lower
+            self.side_number: int = higher - lower
+            self.lower: int = lower
+            self.higher: int = higher
