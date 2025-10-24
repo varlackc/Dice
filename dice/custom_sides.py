94f9ecf17a6322ac36f3a885a545c813d73c8089
varlackc <varlackc@mail.gvsu.edu>
2025-10-24 00:17:34 -0500

    add type hints to the customsides class

--- dice/custom_sides.py
+++ dice/custom_sides.py
@@ -15,9 +15,9 @@ class CustomSides(Die):
         https://docs.python.org/3.7/library/functions.html#isinstance
         """
         if (isinstance(sides, int)):
-            self.die_Type = "custom_sided"
-            self.face = 1
-            self.side_number = sides
+            self.die_Type: str = "custom_sided"
+            self.face: int = 1
+            self.side_number: int = sides
         else:
             raise TypeError(
               "The custom sided die must take an interger as an input"
