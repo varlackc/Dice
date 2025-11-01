db47a91ed4688c4a7ec63a7328939400d42ae122
varlackc <varlackc@mail.gvsu.edu>
2025-11-01 12:16:16 -0500

    improve the init method in the D10 class by adding type hints

--- dice/d10.py
+++ dice/d10.py
@@ -8,7 +8,7 @@ class D10(Die):
     Args:
         Die (_type_): _description_
     """
-    def __init__(self):
+    def __init__(self) -> None:
         self.die_Type: str = "d10"
         self.face: int = 1
         self.side_number: int = 10
