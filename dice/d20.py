a6960a15769debf6f7f0feb8d6bca0c72fb02a82
varlackc <varlackc@mail.gvsu.edu>
2025-11-01 12:21:40 -0500

    Improve the init method by adding type hints in the D20 Class

--- dice/d20.py
+++ dice/d20.py
@@ -8,7 +8,7 @@ class D20(Die):
     Args:
         Die (_type_): _description_
     """
-    def __init__(self):
+    def __init__(self) -> None:
         self.die_Type: str = "d20"
         self.face: int = 1
         self.side_number: int = 20
