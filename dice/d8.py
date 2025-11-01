e9e9f53d76be48bbf264f86fccf7448e154a6153
varlackc <varlackc@mail.gvsu.edu>
2025-11-01 12:13:49 -0500

    Improve the init method in the D8 class by adding type hints

--- dice/d8.py
+++ dice/d8.py
@@ -8,7 +8,7 @@ class D8(Die):
     Args:
         Die (_type_): _description_
     """
-    def __init__(self):
+    def __init__(self) -> None:
         self.die_Type: str = "d8"
         self.face: int = 1
         self.side_number: int = 8
