954222047b0d15e6e8232433885fe76db4a37b50
varlackc <varlackc@mail.gvsu.edu>
2025-11-09 22:31:56 -0600

    add type hints to the test_roll method

--- tests/test_d8_die.py
+++ tests/test_d8_die.py
@@ -9,7 +9,7 @@ class TestD8(unittest.TestCase):
     Args:
         unittest (_type_): _description_
     """
-    def test_roll(self):
+    def test_roll(self) -> None:
         # verify an output is present
         self.assertIsNotNone(D8().roll())
 
