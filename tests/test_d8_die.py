0f5b768b8b9f585aa8c8e7e76ee61506b2dd5901
varlackc <varlackc@mail.gvsu.edu>
2025-11-12 00:40:40 -0600

    add type hints to the test_die_Type method

--- tests/test_d8_die.py
+++ tests/test_d8_die.py
@@ -29,6 +29,6 @@ class TestD8(unittest.TestCase):
         # verify that the number of sides are correct
         self.assertEqual(D8().side_number, 8)
 
-    def test_die_Type(self):
+    def test_die_Type(self) -> None:
         # verify that the die type is correct
         self.assertEqual(D8().die_Type, "d8")
